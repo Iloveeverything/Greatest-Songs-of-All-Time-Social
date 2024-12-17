2b27c4bc28fc55df5260a7fe0ad92bccccd11406
Mike Thurstlic <mikethurstlic@Mikes-MacBook-Air.local>
2024-12-17 17:35:18 -0500

    mikeDay1

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+const uri = "mongodb+srv://thurstlic7:chickenandwafflesarethebest@teamscratch.lilc7.mongodb.net/?retryWrites=true&w=majority&appName=teamScratch";
+
+mongoose.connect(uri)
+.then(() => {
+    console.log('Connected to MongoDB everybody');
+    app.listen(8080, () =>{
+        console.log('Server is running on port 3000')
+    })
+})
+.catch((error) => {
+    console.error('MongoDB connection error.', error)
+})
