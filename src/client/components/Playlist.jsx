9aa5e1c51627b04fa499628d1dd5bd580aad908d
Carlaurence <ingeniero.indbermeo@gmail.com>
2024-12-17 16:38:49 -0500

    test1

--- src/client/components/Playlist.jsx
+++ src/client/components/Playlist.jsx
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from 'react'; 
-
+import React, { useState, useEffect } from 'react';
 
 const Playlist = () => {
-
-    return (
-        <div className='playlist-component'>
-
-        </div>
-    );
+  return (
+    <div className='playlist-component'>
+      <h1>Welcome from playlist component</h1>
+    </div>
+  );
 };
 
-export default Playlist; 
+export default Playlist;
