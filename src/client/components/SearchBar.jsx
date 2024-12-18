d499f1e20e1a6f2d8191cb062d830278128d7cb8
Salisu Ibrahim <salisu.ibrahim15@yahoo.com>
2024-12-17 22:36:32 -0500

    completed designing the searchBar

--- src/client/components/SearchBar.jsx
+++ src/client/components/SearchBar.jsx
@@ -1,13 +1,21 @@
-import React, { useState, useEffect } from 'react'; 
-
-
+import React, { useState, useEffect } from 'react';
+import { FaMagnifyingGlass } from 'react-icons/fa6'; // Importing a search icon from react-icons
+import './SearchBar.css'; // Importing the CSS file for styling
 const SearchBar = () => {
-
-    return (
-        <div className='search-bar-component'>
-
-        </div>
-    );
+  // Functional component for a search bar
+  const { input, setInput } = useState(''); // State to handle the input value
+  // Function to fetch data based on the input value
+  const fetchData = (value) => {};
+  return (
+    <div className='search-bar-component'>
+      <input
+        placeholder='Type to search song...'
+        // value={input}
+        // onChange={(e) => setInput(e.target.value)}
+      />
+      <FaMagnifyingGlass id='search-icon' />
+    </div>
+  );
 };
 
-export default SearchBar; 
+export default SearchBar;
