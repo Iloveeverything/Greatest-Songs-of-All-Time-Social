b510a1ee879503ef98e296777855a950d58d8484
Cindy Rodríguez-Llivipuma <csrl23@Cindys-MacBook-Pro.local>
2024-12-18 15:07:36 -0500

    server updated

--- server.js
+++ server.js
@@ -3,28 +3,36 @@ dotenv.config();
 
 const express = require('express');
 const app = express();
-
+const PORT = 8080; 
 
 const mongoose = require('mongoose');
+
+const controller = require('./src/server/controller'); // import controller
+const router = require('./src/server/routes'); // import router
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-
+//connect to MongoDB 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log('Connected to MongoDB everybody');
-    app.listen(3000, () =>{
-        console.log('Server is running on port 3000')
+    app.listen(PORT, () =>{
+        console.log(`Server is running on port ${PORT}`)
     });
 })
 .catch((error) => {
     console.error('MongoDB connection error.', error);
-})
+}); 
 
+// mount router at the /songs route 
+app.use('/playlist', router); 
+
+// unknown route handler 
 app.use((req, res) => res.sendStatus(404));
 
+// global error handler middleware 
 app.use((err, req, res, next) => {
     const defaultErr={
         log: 'Express error handler caught unknown error',
@@ -41,5 +49,5 @@ app.use((err, req, res, next) => {
     console.error(`Request URL: ${req.originalUrl}`);
 
     res.status(errorDetails.status).json(errorDetails.message);
-})
+}); 
 
