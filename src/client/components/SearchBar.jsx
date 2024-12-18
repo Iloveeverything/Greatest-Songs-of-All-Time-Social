6b6055065692d9dd63a4df495732f68c608943f5
Salisu Ibrahim <salisu.ibrahim15@yahoo.com>
2024-12-18 16:27:17 -0500

    working on handleChange

--- src/client/components/SearchBar.jsx
+++ src/client/components/SearchBar.jsx
@@ -1,17 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { FaMagnifyingGlass } from 'react-icons/fa6'; // Importing a search icon from react-icons
 import './SearchBar.css'; // Importing the CSS file for styling
+import './Playlist';
 const SearchBar = () => {
   // Functional component for a search bar
-  const { input, setInput } = useState(''); // State to handle the input value
+  const [input, setInput] = useState(''); // State to handle the input value
+
+  //   console.log(input);
   // Function to fetch data based on the input value
-  const fetchData = (value) => {};
+  const getMusic = async (value) => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  const handleChange = (value) => {
+    setInput(value);
+    getMusic(value);
+  };
   return (
     <div className='search-bar-component'>
       <input
         placeholder='Type to search song...'
-        // value={input}
-        // onChange={(e) => setInput(e.target.value)}
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
       />
       <FaMagnifyingGlass id='search-icon' />
     </div>
